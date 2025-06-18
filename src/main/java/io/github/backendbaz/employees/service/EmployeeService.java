fbaf459171ecd0f00ddce87864b11003f7a8e0a9
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-06-18 01:38:40 -0700

    feat: implement the EmployeeRequest method
    feat: implement save, findById, update and deleteById methods in the Service layer

--- src/main/java/io/github/backendbaz/employees/service/EmployeeService.java
+++ src/main/java/io/github/backendbaz/employees/service/EmployeeService.java
@@ -1,11 +1,22 @@
 package io.github.backendbaz.employees.service;
 
 import io.github.backendbaz.employees.entity.Employee;
+import io.github.backendbaz.employees.request.EmployeeRequest;
 
 import java.util.List;
 
 public interface EmployeeService {
 
     List<Employee> findAll();
+
+    Employee findById(long id);
+
+    Employee save(EmployeeRequest employeeRequest);
+
+    Employee update(long id, EmployeeRequest employeeRequest);
+
+    Employee convertToEmployee(long id, EmployeeRequest employeeRequest);
+
+    void deleteById(long id);
 
 }
--- src/main/java/io/github/backendbaz/employees/service/EmployeeServiceImp.java
+++ src/main/java/io/github/backendbaz/employees/service/EmployeeServiceImp.java
@@ -2,8 +2,10 @@ package io.github.backendbaz.employees.service;
 
 import io.github.backendbaz.employees.dao.EmployeeDAO;
 import io.github.backendbaz.employees.entity.Employee;
+import io.github.backendbaz.employees.request.EmployeeRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -20,6 +22,39 @@ public class EmployeeServiceImp implements EmployeeService {
     @Override
     public List<Employee> findAll() {
         return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(long id) {
+        return employeeDAO.findById(id);
+    }
+
+    @Transactional
+    @Override
+    public Employee save(EmployeeRequest employeeRequest) {
+        return employeeDAO.save(convertToEmployee(0, employeeRequest));
+    }
+
+    @Transactional
+    @Override
+    public Employee update(long id, EmployeeRequest employeeRequest) {
+        return employeeDAO.save(convertToEmployee(id, employeeRequest));
+    }
+
+    @Override
+    public Employee convertToEmployee(long id, EmployeeRequest employeeRequest) {
+        return new Employee(
+                id,
+                employeeRequest.getFirstName(),
+                employeeRequest.getLastName(),
+                employeeRequest.getEmail()
+        );
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(long id) {
+        employeeDAO.deleteById(id);
     }
 
 }
