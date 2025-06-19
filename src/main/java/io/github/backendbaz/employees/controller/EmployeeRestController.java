d1f01a6a047f47c3c1b0fab5130552f594fe9fda
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-06-18 22:40:13 -0700

    feat: implement the '/api/employees/{id}' endpoint in the EmployeeRestController
    docs: add the file of http requests for testing REST API

--- src/main/java/io/github/backendbaz/employees/controller/EmployeeRestController.java
+++ src/main/java/io/github/backendbaz/employees/controller/EmployeeRestController.java
@@ -2,17 +2,16 @@ package io.github.backendbaz.employees.controller;
 
 import io.github.backendbaz.employees.entity.Employee;
 import io.github.backendbaz.employees.service.EmployeeService;
+import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/employees")
+
 public class EmployeeRestController {
 
     private final EmployeeService employeeService;
@@ -26,6 +25,12 @@ public class EmployeeRestController {
     @GetMapping
     public List<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/{id}")
+    public Employee findEmployeeById(@PathVariable @Min(value = 1) long id) {
+        return employeeService.findById(id);
     }
 
 }
